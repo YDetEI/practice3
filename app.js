f0b0bf91eae418fc27688de872d2181581d7d59a
darvish2 <60345474+darvish2@users.noreply.github.com>
2021-11-10 19:39:44 +0900

    up

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
 
 var path = require('path');
@@ -10,52 +10,55 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-
-
-//1
-app.get('/', function(req, res) {
-    res.send('hello world');
-});
-app.get('/about', function(req, res) {
-    res.send('about this page!');
-});
-
-//2
-app.get('/users/:name?', function(req, res) {
-    if (req.params.name) {
-        res.send('hello, ' + req.params.name);
-    } else {
-        res.send('hello, nobody!');
-    }
-});
-app.get('/items/:id([0-9]+)', function(req, res) {
-    res.send('item no: ' + req.params.id);
-});
-
-//3
-app.use(express.static(__dirname + '/text'));
-app.get('/1.txt', function(req, res) {
-    res.sendfile(__dirname + '/text/1.txt');
-});
+var bodyParser = require('body-parser');
 
 //#8
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// //1
+// app.get('/', function(req, res) {
+//     res.send('hello world');
+// });
+// app.get('/about', function(req, res) {
+//     res.send('about this page!');
+// });
+
+// //2
+// app.get('/users/:name?', function(req, res) {
+//     if (req.params.name) {
+//         res.send('hello, ' + req.params.name);
+//     } else {
+//         res.send('hello, nobody!');
+//     }
+// });
+// app.get('/items/:id([0-9]+)', function(req, res) {
+//     res.send('item no: ' + req.params.id);
+// });
+
+// //3
+// app.use(express.static(__dirname + '/text'));
+// app.get('/1.txt', function(req, res) {
+//     res.sendfile(__dirname + '/text/1.txt');
+// });
+
+
+
 
 // middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/text'));
-app.param('id', function(req, res, next, id) {
-    var users = ['hara', 'shinjo', 'tatsunami'];
-    req.params.name = users[id];
-    next();
+
+app.get('/new', function(req, res) {
+    res.render('new');
 });
-app.get('/1/:id', function(req, res) {
-    res.send('hello ' + req.params.name);
-});
-app.get('/bye/:id', function(req, res) {
-    res.send('bye ' + req.params.name);
+app.post('/create', function(req, res) {
+    res.send(req.body.name);
+
 });
 
 
